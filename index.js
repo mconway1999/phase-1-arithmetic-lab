32f711e31fbfb34255f231e9dc21c6e39e992755
Madeline Conway <conway.maddy@yahoo.com>
2024-08-27 13:13:32 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = (num1 * num2)
+
+
+
+const random = Math.floor(Math.random() * 2) + 1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = (num3 % num4)
+
+const max = Math.max(20, 2, 3, 4, 5);
